b6b3b6e829f210f2f40e767723d3ac56fa602bbd
wilymonkey <codeberg@rohit.top>
2025-07-22 09:55:03 +1000

    build to rely on text file

--- fetch_and_build.sh
+++ fetch_and_build.sh
@@ -2,8 +2,10 @@
 
 DOCKER_NAME="$1"
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+LAST_COMMIT_FILE="./last_commit.txt"
+
 pushd "$SCRIPT_DIR"
-OLD_COMMIT=$(git rev-parse HEAD)
+OLD_COMMIT=$(cat "$LAST_COMMIT_FILE")
 git pull
 NEW_COMMIT=$(git rev-parse HEAD)
 
@@ -11,6 +13,7 @@ if [ "$OLD_COMMIT" != "$NEW_COMMIT" ]; then
     echo "Changes detected! Pulling and running script..."
     bun run prod
     docker restart "$DOCKER_NAME"
+    git rev-parse HEAD > "$LAST_COMMIT_FILE"
 else
     echo "No changes."
 fi
